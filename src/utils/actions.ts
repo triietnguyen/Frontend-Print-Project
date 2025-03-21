7260d5c6cac8d152421b9dd81cead2803ab186dc
triietnguyen <trietnguyenhoangminh@gmail.com>
2025-03-21 16:28:17 +0700

    login page

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,21 +1,33 @@
 'use server'
-import {signIn} from "@/auth";
+import { signIn } from "@/auth";
 
 export async function authenticate(email: string, password: string) {
     try {
         const r = await signIn("credentials", {
-            username: email,
+            email: email,
             password: password,
             // callbackUrl: "/",
             redirect: false,
         })
         return r
     } catch (error) {
-        return { "error": "Incorrect username or password"}
-        // if (error.cause.err instanceof InvalidLoginError) {
-        //     return {"error": "Incorrect username or password"}
-        // } else {
-        //     throw new Error("Failed to authenticate")
-        // }
+        if ((error as any).type === "InvalidEmailPasswordError") {
+            return {
+                error: (error as any).type,
+                code: 1
+            }
+        }
+        else if ((error as any).type === "InactiveAccountError") {
+            return {
+                error: (error as any).type,
+                code: 2
+            }
+        } else {
+            return {
+                error: "Internal server",
+                code: 2
+            }
+        }
+
     }
 }
